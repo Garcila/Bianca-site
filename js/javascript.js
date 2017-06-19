cb6269b465fad2be4e053421de089e3ad291f1e4
German Arcila <germanarcila@gmail.com>
2017-06-19 16:56:08 -0400

    added images

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,6 @@ const start = () => {
 
   //set up selections
   const photoContainer = document.querySelector('.big-photo').offsetHeight;
-  const bigPhoto = document.querySelector('.big-photo');
   const square = document.querySelector('.square');
   const title = document.querySelector('.title');
   const nav = document.querySelector('nav');
@@ -13,30 +12,18 @@ const start = () => {
     element: document.querySelector('.point'),
     handler: (direction) => {
       direction === 'down' ? nav.className = 'sticky' : nav.className = 'hidden_menu';
-      // nav.className='sticky';
     },
 
-    offset: 10,
+    offset: '20px',
   });
 
   //parallax the logo and name in the hero image
   function parallax() {
-    const wScroll = window.scrollY;
+    const verticalScroll = window.scrollY;
 
-    // console.log(wScroll);
-    if (wScroll <= photoContainer && wScroll < 475) {
-      square.style.transform = `translate(0%, ${wScroll / 1.6}% )`;
-      title.style.transform = `translate(${wScroll / 10}%, ${wScroll / 3.7}%)`;
-    }
-
-    // else if (wScroll >= 476) {
-    //   // bigPhoto.className = 'big-photo-sm';
-    //   // square.style.transform = 'translate(0, 0)';
-    // }
-
-    if (wScroll === 0) {
-      // console.log('zero');
-      bigPhoto.className = 'big-photo';
+    if (verticalScroll <= photoContainer && verticalScroll < 400) {
+      square.style.transform = `translate(0%, ${verticalScroll / 1.9}% )`;
+      title.style.transform = `translate(${verticalScroll / 8}%, ${verticalScroll / 4.9}%)`;
     }
   }
 
