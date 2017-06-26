07256e8c8d929db99f935431cc164b6a807eed57
German Arcila <germanarcila@gmail.com>
2017-06-26 10:30:31 -0400

    working on parallax ios

--- js/javascript.js
+++ js/javascript.js
@@ -1,33 +1,32 @@
 /* eslint-env browser */
 const start = () => {
+    //set up selections
+    const photoContainer = document.querySelector('.big-photo').offsetHeight;
+    const square = document.querySelector('.logo');
+    const title = document.querySelector('.title');
+    const nav = document.querySelector('nav');
 
-  //set up selections
-  const photoContainer = document.querySelector('.big-photo').offsetHeight;
-  const square = document.querySelector('.logo');
-  const title = document.querySelector('.title');
-  const nav = document.querySelector('nav');
+    //sticky navigation
+    const waypoint = new Waypoint({
+        element: document.querySelector('.waypoint'),
+        handler: (direction) => {
+            direction === 'down' ? nav.className = 'sticky' : nav.className = 'hidden_menu';
+        },
 
-  //sticky navigation
-  const waypoint = new Waypoint({
-    element: document.querySelector('.waypoint'),
-    handler: (direction) => {
-      direction === 'down' ? nav.className = 'sticky' : nav.className = 'hidden_menu';
-    },
+        offset: '80px',
+    });
 
-    offset: '80px',
-  });
+    //parallax the logo and name in the hero image
+    function parallax() {
+        const verticalScroll = window.scrollY;
 
-  //parallax the logo and name in the hero image
-  function parallax() {
-    const verticalScroll = window.scrollY;
-
-    if ((verticalScroll <= photoContainer) && (verticalScroll < 250)) {
-      square.style.transform = `translate(0%, ${verticalScroll / 1.4}% )`;
-      title.style.transform = `translate(${verticalScroll * 0.2}%, ${verticalScroll * -0.1}%)`;
+        if ((verticalScroll <= photoContainer) && (verticalScroll < 250)) {
+            square.style.transform = `translate(0%, ${verticalScroll / 1.4}% )`;
+            title.style.transform = `translate(${verticalScroll * 0.2}%, ${verticalScroll * -0.1}%)`;
+        }
     }
-  }
 
-  window.addEventListener('scroll', parallax);
+    window.addEventListener('scroll', parallax);
 };
 
 start();
