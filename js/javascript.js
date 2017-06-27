9ee63970eb851d2f4c90d4f0487e0ac2595c9684
German Arcila <germanarcila@gmail.com>
2017-06-27 11:01:08 -0400

    re-enabled js and starting adjusting. working well on laptop

--- js/javascript.js
+++ js/javascript.js
@@ -1,39 +1,39 @@
 /* eslint-env browser */
-const start = () => {
-    //set up selections
-    const photoContainer = document.querySelector('.hero').offsetHeight;
-    const square = document.querySelector('.logo');
-    const title = document.querySelector('.title');
-    const nav = document.querySelector('nav');
+// const start = () => {
+//set up selections
+const photoContainer = document.querySelector('.hero').offsetHeight;
+const square = document.querySelector('.logo');
+const title = document.querySelector('.title');
+const nav = document.querySelector('nav');
 
-    //sticky navigation
-    const waypoint = new Waypoint({
-        element: document.querySelector('.waypoint'),
-        handler: (direction) => {
-            if (direction === 'down') {
-                nav.className = 'sticky';
-                document.body.style.paddingTop = nav.offsetHeight + 'px';
-            } else {
-                nav.className = 'hidden_menu';
-                document.body.style.paddingTop = 0;
-            }
-        },
-
-        offset: `${nav.offsetHeight}px`,
-    });
-
-    //parallax the logo and name in the hero image
-    function parallax() {
-        const verticalScroll = window.scrollY;
-
-        if ((verticalScroll <= photoContainer) && (verticalScroll < 250)) {
-            square.style.transform = `translate(0%, ${verticalScroll / 1.4}% )`;
-            title.style.transform = `translate(0%, ${verticalScroll * 1.3}% )`;
-            // title.style.transform = `translate(${verticalScroll * 0.2}%, ${verticalScroll * -0.1}%)`;
+//sticky navigation
+const waypoint = new Waypoint({
+    element: document.querySelector('.waypoint'),
+    handler: (direction) => {
+        if (direction === 'down') {
+            nav.className = 'sticky';
+            document.body.style.paddingTop = nav.offsetHeight + 'px';
+        } else {
+            nav.className = 'hidden_menu';
+            document.body.style.paddingTop = 0;
         }
+    },
+
+    offset: `${nav.offsetHeight}px`,
+});
+
+//parallax the logo and name in the hero image
+function parallax() {
+    const verticalScroll = window.scrollY;
+
+    if ((verticalScroll <= photoContainer) && (verticalScroll < 250)) {
+        square.style.transform = `translate(0%, ${verticalScroll / 1.4}% )`;
+        title.style.transform = `translate(0%, ${verticalScroll * 1.3}% )`;
+        // title.style.transform = `translate(${verticalScroll * 0.2}%, ${verticalScroll * -0.1}%)`;
     }
+}
 
-    window.addEventListener('scroll', parallax);
-};
+window.addEventListener('scroll', parallax);
+// };
 
-start();
+// start();
