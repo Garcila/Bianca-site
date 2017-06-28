e8a83b61cee7a15886c3f625888ad7d09e79adf0
German Arcila <germanarcila@gmail.com>
2017-06-28 10:40:36 -0400

    js working on all devices, prefixes added, cleaned html, css and javascript

--- js/javascript.js
+++ js/javascript.js
@@ -2,15 +2,13 @@
 
 const nav = document.querySelector('.menu');
 const waypoint = document.querySelector('.waypoint');
-// let topOfNav = nav.offsetTop;
 
 function fixNav() {
     const verticalScroll = window.scrollY;
 
-    // if (window.scrollY >= topOfNav) {
     if (verticalScroll >= photoContainer) {
         document.body.classList.add('fixed-nav');
-        waypoint.style.paddingTop = nav.offsetHeight + 'px';
+        waypoint.style.paddingTop = `${nav.offsetHeight}px`;
     } else {
         document.body.classList.remove('fixed-nav');
         document.body.style.paddingTop = 0;
@@ -22,8 +20,6 @@ window.addEventListener('touchmove', fixNav, false);
 
 //set up selections
 const photoContainer = document.querySelector('.hero').offsetHeight;
-const logo = document.querySelector('.logo');
-const title = document.querySelector('.title');
 const letters = document.querySelector('.letters');
 
 // //parallax the logo and name in the hero image
