93bb90a086449ad5cef5d93c01e5ad7450e06d07
German Arcila <germanarcila@gmail.com>
2017-06-27 23:12:12 -0400

    movement in hero image and fixed nav, not using waypoints, just vanilla js

--- js/javascript.js
+++ js/javascript.js
@@ -19,29 +19,11 @@ window.addEventListener('touchmove', fixNav, false);
 window.addEventListener('touchmove', () => { console.log('zapato'); }, false);
 window.addEventListener('scroll', () => { console.log('zapato'); }, false);
 
-// const start = () => {
 //set up selections
 const photoContainer = document.querySelector('.hero').offsetHeight;
 const logo = document.querySelector('.logo');
 const title = document.querySelector('.title');
 const letters = document.querySelector('.letters');
-// const nav = document.querySelector('nav');
-
-// //sticky navigation
-// const waypoint = new Waypoint({
-//     element: document.querySelector('.waypoint'),
-//     handler: (direction) => {
-//         if (direction === 'down') {
-//             nav.className = 'sticky';
-//             document.body.style.paddingTop = nav.offsetHeight + 'px';
-//         } else {
-//             nav.className = 'hidden_menu';
-//             document.body.style.paddingTop = 0;
-//         }
-//     },
-
-//     offset: `${nav.offsetHeight}px`,
-// });
 
 // //parallax the logo and name in the hero image
 function parallax() {
@@ -49,13 +31,8 @@ function parallax() {
 
     if ((verticalScroll <= photoContainer)) {
         letters.style.transform = `translate(0%, ${verticalScroll / 8}% )`;
-        // title.style.transform = `translate(0%, ${verticalScroll / 1.6}% )`;
-        // title.style.transform = `translate(${verticalScroll * 0.2}%, ${verticalScroll * -0.1}%)`;
     }
 }
 
 window.addEventListener('touchmove', parallax, false);
 window.addEventListener('scroll', parallax, false);
-// };
-
-// start();
